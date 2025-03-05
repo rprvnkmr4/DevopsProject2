74ecd147369343f4bd129c5d8d170a20a8475c38
PK <59316816+rprvnkmr4@users.noreply.github.com>
2025-03-05 18:35:00 +0530

    Update provider.tf

--- terraform_code/eks_code/provider.tf
+++ terraform_code/eks_code/provider.tf
@@ -2,7 +2,7 @@ locals {
   region = "ap-south-1"
   name   = "amazon-prime-cluster"
   vpc_cidr = "10.0.0.0/16"
-  azs      = ["us-east-1a", "us-east-1b"]
+  azs      = ["ap-south-1a", "ap-south-1b"]
   public_subnets  = ["10.0.1.0/24", "10.0.2.0/24"]
   private_subnets = ["10.0.3.0/24", "10.0.4.0/24"]
   intra_subnets   = ["10.0.5.0/24", "10.0.6.0/24"]
